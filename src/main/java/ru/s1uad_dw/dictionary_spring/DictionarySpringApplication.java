a534864cb03eb1eaafe36a11203c09bf2fcb37fa
s1uad-dw <s1uad.dw@gmail.com>
2024-02-15 08:13:03 +0300

    save

---
+++ src/main/java/ru/s1uad_dw/dictionary_spring/DictionarySpringApplication.java
@@ -0,0 +1,12 @@
+package ru.s1uad_dw.dictionary_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionarySpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DictionarySpringApplication.class, args);
+	}
+
+}
